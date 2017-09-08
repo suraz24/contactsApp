88cb0d43471fbdf6c59f94a8bf649f0a3db79f1a
Krishna Adhikari <kadhikar@unico-aus.unico.com.au>
2017-09-08 11:50:38 +1000

    reverted back to the working version

--- src/components/index.js
+++ src/components/index.js
@@ -3,16 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './welcome.js';
-import Contacts from './contacts.js'
+import Form from './form.js';
 
-var Contacts = React.createClass({
-    render: function() {
-        return(
-            <div>
-                <Contacts />
-            </div>
-        );
-    }
 
-})
-ReactDOM.render(<Home />, document.getElementById('app'));
+ReactDOM.render(<Form />, document.getElementById('app'));
