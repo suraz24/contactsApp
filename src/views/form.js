1992f97784f1415964cfb249cc631a0bd14acf17
Lauren <lchia@unico-aus.unico.com.au>
2017-09-28 10:31:06 +1000

    Form validation integrated

--- src/views/form.js
+++ src/views/form.js
@@ -5,49 +5,105 @@ import Button from 'muicss/lib/react/button';
 import Input from 'muicss/lib/react/input';
 import GoBack from '../components/goBack.js';
 import formStyle from './form.css';
+import FormErrors from './formErrors.js';
 
 class ContactsForm extends React.Component{
   constructor(props){
-		super(props);
+    super(props);
         this.state= {
-              firstname: this.props.contact.firstname ||"",
-              lastname: this.props.contact.lastname || "",
-              workphone: this.props.contact.workphone || "",
-              mobile: this.props.contact.mobile || "",
+          firstname: this.props.contact.firstname ||"",
+          lastname: this.props.contact.lastname || "",
+          workphone: this.props.contact.workphone || "",
+          mobile: this.props.contact.mobile || "", 
+          formErrors: { 
+                  firstname: "",
+                  lastname: "",
+                  workphone: "",
+                  mobile: ""
+                },
+          firstnameValid: false,
+          lastnameValid: false,
+          workphoneValid:false,
+          mobileValid: false,
+          firstnameValid: this.props.contact.firstname.match(/^[A-Za-z -]+$/),
+          lastnameValid: this.props.contact.lastname.match(/^[A-Za-z -]+$/),
+          workphoneValid:this.props.contact.workphone.match(/^[0-9]{8,8}$/),
+          mobileValid: this.props.contact.mobile.match(/^[0-9]{10,10}$/),
+          formValid: false
         }
         this.changeValues=this.changeValues.bind(this);
-	}
+  }
+  validateField(fieldName, value) {
+  let fieldValidationErrors = this.state.formErrors;
+  let firstnameValid = this.state.firstnameValid;
+  let lastnameValid = this.state.lastnameValid;
+  let workphoneValid = this.state.workphoneValid;
+  let mobileValid = this.state.mobileValid;
+
+  switch(fieldName) {
+    case 'firstname':
+      firstnameValid = value.match(/^[A-Za-z -]+$/); //pattern for regular letters and spaces
+      fieldValidationErrors.firstname = firstnameValid ? '' : 'First name must not contain symbols or numbers';
+      break;
+    case 'lastname':
+      lastnameValid = value.match(/^[A-Za-z -]+$/); //pattern for regular letters and spaces
+      fieldValidationErrors.lastname = lastnameValid ? '' : 'Last name must not contain symbols or numbers';
+      break;
+    case 'workphone':
+      workphoneValid = value.match(/^[0-9]{8,8}$/);
+      fieldValidationErrors.workphone = workphoneValid ? '': 'Work phone must be 8 numeric digits';
+      break;
+    case 'mobile':
+      mobileValid = value.match(/^[0-9]{10,10}$/);
+      fieldValidationErrors.mobile = mobileValid ? '': 'Mobile must be 10 numeric digits';
+      break;
+    default:
+      break;
+  }
+  this.setState({formErrors: fieldValidationErrors,
+                  firstnameValid: firstnameValid,
+                  lastnameValid: lastnameValid,
+                  workphoneValid: workphoneValid,
+                  mobileValid: mobileValid
+                }, this.validateForm);
+  }
+
+  validateForm() {
+    console.log(this.state.firstnameValid);
+  this.setState({formValid: this.state.firstnameValid && this.state.lastnameValid && this.state.workphoneValid && this.state.mobileValid});
+  }
+
   adding(e){
     e.preventDefault();
     this.props.action(this.state);
   }
 
   changeValues(e){
-    this.setState({
-        [e.target.name]: e.target.value
-    });
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ // takes callback function to validate after user types in field
+        [name]: value},
+        () => {this.validateField(name,value)});
+  }
 
-  }
   render() {
-    return (
-      <div>
-          <div id='formHeading' className="mui--text-display3">{this.props.heading}</div>
-            <GoBack />
-            <form className='ContactForm' id='ContactForm' noValidate = "novalidate">
-              <Input label="First Name *" floatingLabel={true} type="text" autoComplete="off" id="firstname" name="firstname" value={this.state.firstname} onChange={this.changeValues} />
-              <div id="firstnameError"></div>
-              <Input label="Last Name *" floatingLabel={true} type="text" autoComplete="off" id="lastname"name="lastname" value={this.state.lastname} onChange={this.changeValues}/>
-              <div id="lastnameError"></div>
-              <Input label="Work Phone" floatingLabel={true} type="text" pattern="^[0-9]{8,8}" autoComplete="off" id="workphone" name="workphone" value={this.state.workphone} onChange={this.changeValues} />
-              <div id="workphoneError"></div>
-              <Input label="Mobile *" floatingLabel={true} type="text" pattern="^[0-9]{10,10}" autoComplete="off" id="mobile" name="mobile" value={this.state.mobile} onChange={this.changeValues} />
-              <div id="mobileError"></div>
-              <Button variant="raised" color="primary" className="SaveButton" type="submit" onClick={this.adding.bind(this)} >{this.props.button}</Button>
-            </form>
-          <div id="hint" className="mui--text-dark-secondary">* mandatory fields</div>
-      </div>
-    );
-  }
+        return (
+        <div>
+          <div id='formHeading' className="mui--text-display3">{this.props.heading}</div>      
+          <GoBack />
+          <div id="errorPanel">
+              <FormErrors formErrors={this.state.formErrors} />
+          </div>
+          <form className='ContactForm' noValidate="noValidate">
+            <Input label="First Name" floatingLabel={true} type="text" name="firstname" value={this.state.firstname} autoComplete="off" onChange={this.changeValues} required/>
+            <Input label="Last Name" floatingLabel={true} type="text" name="lastname" value={this.state.lastname} autoComplete="off" onChange={this.changeValues} required/>
+            <Input label="Work Phone" floatingLabel={true} type="text" name="workphone" value={this.state.workphone} autoComplete="off" onChange={this.changeValues} required/>
+            <Input label="Mobile" floatingLabel={true} type="text" name="mobile" value={this.state.mobile} autoComplete="off" onChange={this.changeValues} required/>
+            <Button variant="raised" color="primary" className="SaveButton" type="submit" disabled={!this.state.formValid} onClick={this.adding.bind(this)}>{this.props.button}</Button>
+          </form>
+        </div>
+      );
+   }
 }
 
 ContactsForm.defaultProps = {
