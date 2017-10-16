08c1332448ea8df5b7d982803b55256e95186be1
Lauren <lchia@unico-aus.unico.com.au>
2017-10-16 11:04:44 +1100

    working app after changes for tests

--- src/index.js
+++ src/index.js
@@ -4,21 +4,27 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Routes from './routes.js';
 
-class App extends React.Component {
-    constructor(){
-        super();
-console.log('In app');
-    }
+// class App extends React.Component {
+//     constructor(){
+//         super();
+// console.log('In app');
+//     }
 
-   render() {
-      return (
-         <div>
-             <Router basename="#" >
-                <Routes />
-             </Router>
-         </div>
+//    render() {
+//       return (
+//            <Router basename="#">
+//               <Routes />
+//            </Router>
+//       );
+//    };
+// }
+
+function App() {
+  return (
+           <Router basename="#">
+              <Routes />
+           </Router>
       );
-   }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
