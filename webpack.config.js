b649414019bbd391d8c348fd679878c431279ab9
Krishna Adhikari <kadhikar@unico-aus.unico.com.au>
2017-10-17 15:14:33 +1100

    webpack endpoint change

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 //define the Host URL constants
 var API_URL = {
     development: JSON.stringify('http://contacts-app-be.ap-southeast-2.elasticbeanstalk.com/'),
-    production: JSON.stringify('http://virtserver.swaggerhub.com/suraz/ContactsAPI/1.0.0')
+    production: JSON.stringify('http://contacts-app-be.ap-southeast-2.elasticbeanstalk.com/')
 }
 
 //check the environment
